a3fef85388415c4aadbd18e00768976e4a0c67a1
saladinhodzic <hsaladin06@gmail.com>
2024-07-11 19:29:49 +0200

    Cleaning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,3 @@
-import { FaInstagram } from "react-icons/fa";
-import { CiFacebook } from "react-icons/ci";
-import { CiTwitter } from "react-icons/ci";
 import "./Footer.css";
 
 export default function Footer() {
@@ -8,10 +5,6 @@ export default function Footer() {
     <footer>
       <div className="info">
         <h2>@SAKIley2024</h2>
-        {/* <h5>Stevana Nemanje 47</h5>
-        <h5>Novi Pazar 36300</h5>
-        <h5>Phone:063-842-42-88</h5>
-        <h5>E-mail:hsaladin06@gmail.com</h5> */}
       </div>
     </footer>
   );

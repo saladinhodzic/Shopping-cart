ee19d56f2c7fcb1597015afe762f8d10f26f3ca1
saladinhodzic <hsaladin06@gmail.com>
2024-07-11 18:40:32 +0200

    Finishing the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import "./Header.css";
 import logo from "../../assets/shopping-logo.svg";
+import { FaShoppingCart } from "react-icons/fa";
 export default function Header() {
   return (
     <header>
-      <div className="logo">
-        <img src={logo} alt="" className="shopping-logo" />
+      <div className="wrapper">
+        <div className="logo">
+          <img src={logo} alt="" className="shopping-logo" />
+          <h1>SAKILEY</h1>
+        </div>
+        <div className="listing">
+          <li>Products</li>
+          <li>My Favourites</li>
+          <li>
+            <FaShoppingCart className="icon" />
+          </li>
+        </div>
       </div>
-      <div className="listing"></div>
     </header>
   );
 }

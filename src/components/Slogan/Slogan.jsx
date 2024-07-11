726353cd6eeab3625d7e3a435714d8a9e4f366c7
saladinhodzic <hsaladin06@gmail.com>
2024-07-11 19:32:09 +0200

    Slogan improving

--- src/components/Slogan/Slogan.jsx
+++ src/components/Slogan/Slogan.jsx
@@ -1,11 +1,14 @@
 import "./Slogan.css";
+import { Link } from "react-router-dom";
 export default function Slogan() {
   return (
     <main>
       <div className="wrapper-slogan">
         <div className="content">
           <h1>Discover. Shop. Enjoy.</h1>
-          <h2>See our products.</h2>
+          <h2>
+            See our <Link to="/products">products</Link>.
+          </h2>
         </div>
       </div>
     </main>

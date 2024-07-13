56f8dab068ad52457bb79931f8154048f2de063b
saladinhodzic <hsaladin06@gmail.com>
2024-07-13 19:40:05 +0200

    Changing the json file

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,17 +1,6 @@
-import { useState } from "react";
 import productsJSON from "../../common/products.json";
-import { useEffect } from "react";
 
 export default function Products() {
-  const [products, setProducts] = useState(productsJSON);
-  useEffect(() => {
-    setProducts(
-      products.map((product, index) => {
-        product.id = index + 1;
-        product.stock = Math.floor(Math.random() * 50);
-      })
-    );
-  }, []);
-  console.log(products);
+  console.log(productsJSON);
   return;
 }

9c1a8b3e6110b9ee3e45be302e60616c3458dae5
saladinhodzic <hsaladin06@gmail.com>
2024-07-18 12:20:07 +0200

    Starting to make CartCard

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,20 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-
+import CartCard from "../../components/CartCard/CartCard";
 export default function Cart() {
   const { productsInCart } = useContext(AppContext);
-  console.log(productsInCart);
-  return <></>;
+  return (
+    <div className="wrapper-cart">
+      {productsInCart.map((product) => {
+        return (
+          <CartCard
+            image={product.image_url}
+            title={product.title}
+            stock={product.stock}
+            price={product.price}
+          />
+        );
+      })}
+    </div>
+  );
 }

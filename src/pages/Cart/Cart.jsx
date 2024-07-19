a7ae976c75adbee157681ac16b91ffb13bc6e46e
saladinhodzic <hsaladin06@gmail.com>
2024-07-19 18:34:24 +0200

    Finishing error page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,26 +1,38 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import CartCard from "../../components/CartCard/CartCard";
+import shopPhoto from "../../assets/shop.jpg";
 import "./Cart.css";
+import { Link } from "react-router-dom";
 export default function Cart() {
   const { productsInCart, addToCart } = useContext(AppContext);
   return (
     <div className="wrapper-page">
-      <div className="wrapper-cart">
-        <h1>My Cart</h1>
-        {productsInCart.map((product) => {
-          return (
-            <CartCard
-              image={product.image_url}
-              title={product.title}
-              stock={product.stock}
-              price={product.current_price}
-              description={product.short_description}
-              onClick={() => addToCart(product)}
-            />
-          );
-        })}
-      </div>
+      {productsInCart.length < 1 ? (
+        <div className="error">
+          <img className="error-img" src={shopPhoto} />
+          <h2>No items in cart</h2>
+          <p>
+            Start browsing <Link to="/products">products</Link>
+          </p>
+        </div>
+      ) : (
+        <div className="wrapper-cart">
+          <h1>My Cart</h1>
+          {productsInCart.map((product) => {
+            return (
+              <CartCard
+                image={product.image_url}
+                title={product.title}
+                stock={product.stock}
+                price={product.current_price}
+                description={product.short_description}
+                onClick={() => addToCart(product)}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }

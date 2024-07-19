0643f590ee2dabfb094bb9d8c8d058b498384d8b
saladinhodzic <hsaladin06@gmail.com>
2024-07-19 17:59:22 +0200

    Making the UI looking better

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,3 +1,13 @@
-export default function CartCard() {
-  return;
+import "./CartCard.css";
+export default function CartCard({ image, title, description, stock, price }) {
+  return (
+    <div className="product-cart">
+      <img className="product-image" src={image} />
+      <div className="product-info">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <h2>{price}</h2>
+    </div>
+  );
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,20 +1,25 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import CartCard from "../../components/CartCard/CartCard";
+import "./Cart.css";
 export default function Cart() {
   const { productsInCart } = useContext(AppContext);
   return (
-    <div className="wrapper-cart">
-      {productsInCart.map((product) => {
-        return (
-          <CartCard
-            image={product.image_url}
-            title={product.title}
-            stock={product.stock}
-            price={product.price}
-          />
-        );
-      })}
+    <div className="wrapper-page">
+      <div className="wrapper-cart">
+        <h1>My Cart</h1>
+        {productsInCart.map((product) => {
+          return (
+            <CartCard
+              image={product.image_url}
+              title={product.title}
+              stock={product.stock}
+              price={product.current_price}
+              description={product.short_description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
